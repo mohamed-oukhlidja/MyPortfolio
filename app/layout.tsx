35bd43bdc3c8508560cc0214f57d7decb1678db9
mohamed oukhlidja <mohamadoukhlidja@gmail.com>
2024-11-11 13:40:12 +0100

    update the faicon

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Mohamed oukhlidja | Full-stack dev",
+  description: "Mohamed oukhlidja a full-stack dev with 2 years of experience.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
